ab384844e0fd44fc3dab4144e8a800ae56758f60
João Vieira de Castro <joaovieiradecastro@MacBook-Pro-de-Rui-7.local>
2017-10-31 19:45:02 +0000

    list page update (sidenav and table format)

--- js/side-menu.js
+++ js/side-menu.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "50vw";
+    document.getElementById("mySidenav").style.width = "680px";
    	//document.getElementById("page-top").style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
