03c8ef148087346b65850fbda8e762a445169b48
João Vieira de Castro <joaovieiradecastro@MBP-de-Rui-7.lan>
2017-11-04 15:27:41 +0000

    index fonts and spacings finished

--- js/side-menu.js
+++ js/side-menu.js
@@ -9,3 +9,34 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     //document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
+
+
+$(document).ready(function() {
+  // Select / Unselect all checkboxes
+  var checkboxesOn = false;
+  var checkboxes = [];
+
+  for (var i = 0; i < 7; i++) {
+    checkboxes.push(false);
+  }
+
+  $('.checkbox-top').click(function(){
+    checkboxesOn = !checkboxesOn;
+    if (checkboxesOn)
+      $('img.checkbox').attr("src", "../img/list/checkbox-on.png");
+    else
+      $('img.checkbox').attr("src", "../img/list/checkbox-off.png");
+  });
+
+  $('.checkbox').click(function() {
+    var index = $(this)[0].id - 1;
+    if (checkboxes[index] == false){
+      checkboxes[index] = true;
+      $(this)[0].src = "../img/list/checkbox-on.png";
+    } else {
+      checkboxes[index] = false;
+      $(this)[0].src =  "../img/list/checkbox-off.png";
+    }
+  });
+
+});
